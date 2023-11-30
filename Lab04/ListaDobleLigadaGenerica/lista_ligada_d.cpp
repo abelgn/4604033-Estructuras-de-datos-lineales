4304e76282c75ccf7f64ace6c738bb7a7804601d
Abel García Nájera <abel.garcia.n@gmail.com>
2023-11-30 10:39:16 -0600

    Update lista_ligada_d.cpp

--- Lab04/ListaDobleLigadaGenerica/lista_ligada_d.cpp
+++ Lab04/ListaDobleLigadaGenerica/lista_ligada_d.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-#include "lista_ligada_generica_d.h"
+#include "lista_ligada_generica_d2.h"
 
 using namespace std;
 
@@ -13,26 +13,11 @@ int main() {
 
     for (int i=0; i < 5; i++) {
         le.insertar(i, le.fin());
-        ld.insertar((double) i / 5, ld.fin());
-        lc.insertar((char) (i+97), lc.fin());
+        ld.insertar((double) i / 5.0, ld.fin());
+        lc.insertar((char) (97+i), lc.fin());
     }
 
-    NodoD<int> *p = le.primera();
-    while (p != le.fin()) {
-        cout << le.obtener(p) << endl;
-        p = le.siguiente(p);
-    }
-
-    NodoD<double> *q = ld.primera();
-    while (q != ld.fin()) {
-        cout << ld.obtener(q) << endl;
-        q = ld.siguiente(q);
-    }
-
-
-    NodoD<char> *r = lc.primera();
-    while (r != lc.fin()) {
-        cout << lc.obtener(r) << endl;
-        r = lc.siguiente(r);
-    }
+    cout << le << endl;
+    cout << ld << endl;
+    cout << lc << endl;
 }
