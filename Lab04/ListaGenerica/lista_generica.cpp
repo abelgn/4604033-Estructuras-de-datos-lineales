5837d0948cb94e2905283fd0b28875daa55d4b90
Abel García Nájera <abel.garcia.n@gmail.com>
2023-12-01 09:23:51 -0600

    Update lista_generica.cpp

--- Lab04/ListaGenerica/lista_generica.cpp
+++ Lab04/ListaGenerica/lista_generica.cpp
@@ -15,23 +15,9 @@ int main() {
         li.insertar(i, li.fin());
         ld.insertar((double) i / 5, ld.fin());
         lc.insertar((char) (i+97), lc.fin());
-//        le[i] = i;
-//        ld[i] = (double) i / 5;
-//        lc[i] = (char) (i+97);
     }
 
-    for (int i=0; i < 5; i++) {
-        cout << li[i] << endl;
-    }
-//    cout << li << endl;
-
-    for (int i=0; i < 5; i++) {
-        cout << ld[i] << endl;
-    }
-//    cout << ld << endl;
-
-    for (int i=0; i < 5; i++) {
-        cout << lc[i] << endl;
-    }
-//    cout << lc << endl;
+    cout << li << endl;
+    cout << ld << endl;
+    cout << lc << endl;
 }
