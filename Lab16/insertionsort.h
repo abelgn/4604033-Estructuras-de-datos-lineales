3e353546c414df83dbb395cf16c2b800dad00be6
Abel García Nájera <abel.garcia.n@gmail.com>
2025-11-17 07:59:50 -0600

    Actualización archivos laboratorio 16

---
+++ Lab16/insertionsort.h
@@ -0,0 +1,39 @@
+#ifndef SELECTIONSORT_H
+#define SELECTIONSORT_H
+
+
+long long insertionsort_ni(int A[], int n) {
+    long long num_inst = 0;
+    num_inst++;
+    for (int i = 1; i < n; i++) {
+        int k = A[i];
+        int j = i-1;
+        while ((j >= 0) && (A[j] > k)) {
+            A[j+1] = A[j];
+            j--;
+            num_inst += 4;
+        }
+        A[j+1] = k;
+        num_inst += 5;
+    }
+    return num_inst;
+}
+
+
+void insertionsort(int A[], int n) {
+    for (int i = 1; i < n; i++) {
+        int k = A[i];
+        int j = i-1;
+ 
+        // Se mueven los elementos de A[0..i-1], que son
+        // mayores que k, a una posición hacia la derecha
+        // de su posicion actual
+        while (j >= 0 && A[j] > k) {
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = k;
+    }
+}
+
+#endif // SELECTIONSORT_H
---
+++ Lab16/ordenamiento_t.cpp
@@ -0,0 +1,96 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+#include <chrono>
+
+#include "insertionsort.h"
+
+using namespace std;
+using namespace std::chrono;
+
+
+duration<double> crear_arreglo(int *A, int TAM_ARREGLO, int RANGO_MAX) {
+    high_resolution_clock::time_point inicio = high_resolution_clock::now();
+    for (int i = 0; i < TAM_ARREGLO; i++) {
+        int x = rand() % RANGO_MAX;
+        A[i] = x;
+    }
+    high_resolution_clock::time_point fin = high_resolution_clock::now();
+    duration<double> tiempo = duration_cast<duration<double> >(fin - inicio);
+    return tiempo;
+}
+
+
+duration<double> ordenar_insertion_sort(int* A, int TAM_ARREGLO) {
+    high_resolution_clock::time_point inicio = high_resolution_clock::now();
+    insertionsort(A, TAM_ARREGLO);
+    high_resolution_clock::time_point fin = high_resolution_clock::now();
+    duration<double> tiempo = duration_cast<duration<double> >(fin - inicio);
+    return tiempo;
+}
+
+
+int* copiar_arreglo(int A[], int n) {
+    int *aux = new int[n];
+    for (int i = 0; i < n; i++) {
+        aux[i] = A[i];
+    }
+    return aux;
+}
+
+
+void mostrar_arreglo(int *A, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+}
+
+
+void experimentos(int tam, int reps) {
+
+    int TAM_ARREGLO = tam;
+    int RANGO_MAX = 10 * TAM_ARREGLO;
+    int REPETICIONES = reps;
+    double t_prom_is = 0.0;
+
+    srand((unsigned) time(0));
+    for (int i = 0; i < REPETICIONES; i++) {
+        cout << "*** REPETICION " << i+1 << " ***" << endl;
+        int *A = new int[TAM_ARREGLO];
+        duration<double> tiempo;
+
+        // Arreglo aleatorio
+        cout << "Creando arreglo aleatorio de tamaño " << TAM_ARREGLO << "... ";
+        tiempo = crear_arreglo(A, TAM_ARREGLO, RANGO_MAX);
+        cout << "\tCreado. ";
+        cout << "\tTiempo: " << tiempo.count() << "s" << endl;
+
+        int *aux;
+        aux = copiar_arreglo(A, TAM_ARREGLO);
+        cout << "Iniciando ordenamiento con INSERTION SORT... " << flush;;
+        tiempo = ordenar_insertion_sort(aux, TAM_ARREGLO);
+        cout << "\tOrdenado. ";
+        cout << "\tTiempo: " << tiempo.count() << "s" << endl;
+        t_prom_is = t_prom_is + tiempo.count();
+        delete [] aux;
+        
+        delete [] A;
+        cout << endl;
+    }
+    t_prom_is = t_prom_is / REPETICIONES;
+    cout << "*** TIEMPO PROMEDIO ***" << endl;
+    cout << "Insertion sort:\t" << t_prom_is << endl;
+}
+
+
+int main(int argc, char * argv[]) {
+    if (argc != 3) {
+        cout << "Sintaxis: ordenamiento_t <tamaño_arreglo> <repeticiones>" << endl;
+    } else {
+        experimentos(atoi(argv[1]), atoi(argv[2]));
+    }
+    return EXIT_SUCCESS;
+}
